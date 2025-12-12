17297a43499c729d7b42cbf939aecd9eff7b047a
Devin Fox <devin@Devins-MacBook-Pro.local>
2025-12-11 17:16:45 -0800

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.jpg" type="image/jpeg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
